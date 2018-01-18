eb2d1c3b8e545523a81f5803dae1b32122bb8253
Adam Daniels <adam@mediadrive.ca>
2018-01-18 18:28:29 -0500

    Add documentation for Sinatra extension

--- lib/sinatra/unpoly.rb
+++ lib/sinatra/unpoly.rb
@@ -2,34 +2,57 @@
 require "rack/unpoly/middleware"
 require "delegate"
 
-module Sinatra
+module Sinatra # :nodoc:
+  # The Unpoly extension for Sinatra provides a little bit of sugar to make
+  # Unpoly work seamlessly with Roda.
+  #
+  #
+  # = Example
+  #
+  #   require "sinatra/unpoly"
+  #
+  #   class App < Sinatra::Base
+  #     register Sinatra::Unpoly
+  #
+  #     get "/" do
+  #       if up?
+  #         "Unpoly request!"
+  #       else
+  #         "Not Unpoly :("
+  #       end
+  #     end
+  #   end
+  #
   module Unpoly
     class SinatraInspector < DelegateClass(Rack::Unpoly::Inspector)
-      attr_accessor :response
+      attr_accessor :response # :nodoc:
 
-      def initialize(obj, response)
+      def initialize(obj, response) # :nodoc:
         super(obj)
         @response = response
       end
 
+      # Set the page title.
       def title=(value)
         set_title(response, value)
       end
     end
 
     module Helpers
+      # The ::Rack::Unpoly inspector for this request.
       def unpoly
         SinatraInspector.new(env["rack.unpoly"], response)
       end
       alias up unpoly
 
+      # Determine if this is an Unpoly request.
       def unpoly?
         unpoly.unpoly?
       end
       alias up? unpoly?
     end
 
-    def self.registered(app)
+    def self.registered(app) # :nodoc:
       app.use Rack::Unpoly::Middleware
       app.helpers Unpoly::Helpers
     end
