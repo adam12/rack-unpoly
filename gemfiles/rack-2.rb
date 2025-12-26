de1cce98af357dd1fdbad17f4d5fbe01d8fa98f1
Adam Daniels <adam@mediadrive.ca>
2025-12-26 15:40:54 -0500

    Add dev dependencies to gemfiles

--- gemfiles/rack-2.rb
+++ gemfiles/rack-2.rb
@@ -1,3 +1,6 @@
 eval_gemfile("../Gemfile")
 
 gem "rack", "~> 2"
+gem "ostruct"
+gem "cgi"
+gem "logger"
--- gemfiles/sinatra-3.rb
+++ gemfiles/sinatra-3.rb
@@ -1,3 +1,5 @@
 eval_gemfile("../Gemfile")
 
 gem "sinatra", "~> 3"
+gem "ostruct"
+gem "logger"
